354461358fd88008ba098b7ceb9acbde47d731f0
Gonzalo Sanchez Terrones <gsanchez@usertesting.com>
2025-07-21 12:32:49 +0200

    feat: Adding Ec2 instance

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "example" {
   bucket = "terraform-semantic-release-demo"
   acl    = "private"
 }
+
+resource "aws_ec2_instance" "example" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform Example"
+  }
+}
